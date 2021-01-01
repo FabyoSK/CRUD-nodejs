80dfe7301a1e047d26d2dc88df5b4cd7ecd71888
Fábio Alves <fabioalves.contacto@gmail.com>
2021-01-01 15:11:32 -0100

    Complete the basic routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
 
@@ -13,14 +14,32 @@ app.get('/users',  (req, res ) =>{
 
 app.post('/users', (req, res) => {
   const { name, age, sex, country, ocupation} = req.body
-  const user = { name, age, sex, country, ocupation}
+  const user = {id:uuid(), name, age, sex, country, ocupation}
   users.push(user)
 
-  return req.json(user)
+  return res.json(user)
 })
 
 app.put('/users/:id', (req, res) => {
-  
+  const {id} = req.params
+  const { name, age, sex, country, ocupation} = req.body
+  const user = {id, name, age, sex, country, ocupation}
+
+  const userIndex = users.findIndex(user => user.id === id)
+  if(userIndex < 0){ return res.status(400).json({ERROR: "User not found."})}
+
+  users[userIndex] = user
+
+  return res.json(user)
+})
+app.delete('/users/:id', (req, res) => {
+  const {id} = req.params
+
+  const userIndex = users.findIndex(user => user.id === id)
+  if(userIndex < 0){ return res.status(400).json({ERROR: "User not found."})}
+  users.splice(userIndex, 1)
+
+  return res.status(204).send()
 })
 
 app.listen(3333)
