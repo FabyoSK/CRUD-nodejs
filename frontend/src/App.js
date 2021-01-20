9a1f22d2e94ac017f509aedd9bd1cb693ddec2fe
Fábio Alves <fabioalves.contacto@gmail.com>
2021-01-20 21:54:32 -0100

    ADD useEffects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react'
 import './App.css';
+import api from "./services/api";
+function App() {    
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+      api.get('users').then(response => {
+        setUsers(response.data)
+        console.log(response.data)
+      })
+    }, [])
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    users.map(user => <p>{user}</p>)
+  )
 }
 
 export default App;
